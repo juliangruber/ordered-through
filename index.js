5f0c817af00e56f39d9b7446a0ffdeceb8de218c
Julian Gruber <julian@juliangruber.com>
2013-04-01 23:55:04 +0200

    fixed race condition

--- index.js
+++ index.js
@@ -8,21 +8,42 @@ function orderedThrough (fn) {
   var em = new OrderedEmitter;
   var count = 0;
 
+  var processing = 0;
+  em.on('processing',  function () { processing++ });
+  em.on('!processing', function () { processing-- });
+
   // apply fn to `chunk`
   var input = through(function (chunk) {
+    em.emit('processing');
     var c = Number(count++);
 
     fn(chunk, function (err, res) {
-      if (err) return em.emit('error', err);
+      if (err) {
+        em.emit('!processing');
+        em.emit('error', err);
+        return;
+      }
 
       em.emit('chunk', {
         order : c,
         value : res
       });
+      em.emit('!processing');
     });
   });
 
-  var output = through();
+  var output = through(write, end);
+
+  function write (chunk) { this.queue(chunk); }
+  function end () {
+    if (!processing) return output.queue(null);
+    em.on('!processing', function onProcessing () {
+      if (processing) return;
+      
+      em.removeListener('!processing', onProcessing);
+      output.queue(null);
+    });
+  }
 
   // output ordered chunks
   em.on('chunk', function (chunk) {
